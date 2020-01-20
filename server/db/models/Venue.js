fe68675063bd3c69dd962f12ebf5b59d67df6f68
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-20 11:38:39 -0600

    write model for venue

---
+++ server/db/models/Venue.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('Venue', {
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: type.STRING,
+            allowNull: false
+        },
+        // something like '123 Main Street'
+        address1: {
+            type: type.STRING,
+            allowNull: true,
+        },
+        // something like 'Apt 2'
+        address2: {
+            type: type.STRING,
+            allowNull: true,
+        },
+        city: {
+            type: type.STRING,
+            allowNull: true,
+        },
+        state: {
+            type: type.STRING,
+            allowNull: true,
+        },
+        zip_code: {
+            type: type.INTEGER,
+            allowNull: true,
+        },
+    })
+}
