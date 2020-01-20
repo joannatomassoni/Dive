09b68a8cbbbf8df744ea65e92b40d8298b8781c1
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-20 17:25:23 -0600

    get rid of manual id field creation in models

--- server/db/models/Genre.js
+++ server/db/models/Genre.js
@@ -1,11 +1,5 @@
 module.exports = (sequelize, type) => {
   return sequelize.define('genre', {
-    id: {
-      type: type.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true
-    },
     genre: {
       type: type.STRING,
       allowNull: false
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,11 +1,5 @@
 module.exports = (sequelize, type) => {
     return sequelize.define('user', {
-        id: {
-            type: type.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
         name: {
             type: type.STRING,
             allowNull: false
