21145e42301ccecd0980dada91589bacce0f8e18
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-20 11:10:23 -0600

    cleaned up front end views

--- client/screens/SettingsScreen.js
+++ client/screens/SettingsScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, StyleSheet } from 'react-native';
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+    <ScrollView style={styles.container}>
+    </ScrollView>
+  );
 }
 
 SettingsScreen.navigationOptions = {
   title: 'app.json',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
