5a64b518934223b241a9b672359e0f886241aa58
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-20 15:38:16 -0600

    add index file to compile/export models

---
+++ server/db/models/index.js
@@ -0,0 +1,10 @@
+const CommentModel = require('./Comment');
+const GenreModel = require('./Genre');
+const ShowModel = require('./Show');
+const TypeModel = require('./Type');
+const UserModel = require('./User');
+const VenueModel = require('./Venue');
+
+module.exports = {
+    CommentModel, GenreModel, ShowModel, TypeModel, UserModel, VenueModel
+}
