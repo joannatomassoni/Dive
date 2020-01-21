71fc9caa01bdbc2f83e1108e1ee5c18017499a26
Joy <greeniejoy@gmail.com>
2020-01-21 13:36:58 -0600

    added venue and comment routes

--- server/routes.js
+++ server/routes.js
@@ -7,33 +7,53 @@ router.get('/', function (req, res) {
 })
 
 //GET skeleton route for getting user info
-router.get('/user', function (req, res) {
+router.get('/users', function (req, res) {
   res.send("we are getting users!");
 })
 
 //POST skeleton route to post data to user table 
-router.post('/user', function (req, res) {
+router.post('/users', function (req, res) {
   res.send("We received your user info");
 })
 
 //GET skeleton route for getting ONE band info
-router.get('/band', function (req, res) {
+router.get('/bands', function (req, res) {
   res.send("we are getting band!");
 })
 
 //POST skeleton route to post data to band table for ONE band
-router.post('/band', function (req, res) {
+router.post('/bands', function (req, res) {
   res.send("We received your band info");
 })
 
 //GET skeleton route for getting ONE show info
-router.get('/show', function (req, res) {
+router.get('/shows', function (req, res) {
   res.send("we are getting show!");
 })
 
 //POST skeleton route to post data ONE show table
-router.post('/show', function (req, res) {
+router.post('/shows', function (req, res) {
   res.send("We received your show info");
+})
+
+//GET skeleton route for getting ONE venue info
+router.get('/venues', function (req, res) {
+  res.send("we are getting a venue!");
+})
+
+//POST skeleton route to post data ONE venue table
+router.post('/venues', function (req, res) {
+  res.send("We received your venue info");
+})
+
+//GET skeleton route for getting ONE comment info
+router.get('/comments', function (req, res) {
+  res.send("we are getting comment!");
+})
+
+//POST skeleton route to post data ONE comment table
+router.post('/comments', function (req, res) {
+  res.send("We received your comment info");
 })
 
 module.exports = router;
