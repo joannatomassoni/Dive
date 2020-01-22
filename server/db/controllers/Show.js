202b133076089c6df9e97ff1988ea44d588e7a0c
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-22 12:13:48 -0600

    create Show controller file and pass creteShow to index for compiling

---
+++ server/db/controllers/Show.js
@@ -0,0 +1,23 @@
+const { Show } = require('../sequelize');
+
+// Create show
+const createShow = async (req, res) => {
+    try {
+   
+    }
+    catch (err) {
+        console.log(err);
+        res.send(400);
+    }
+}
+
+
+// Get all upcoming shows
+
+// Update show
+
+// Delete show
+
+module.exports = {
+    createShow,
+}
