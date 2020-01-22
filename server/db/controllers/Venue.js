2da3098eaf220b1847ed2b27e1d49b6adbb23c06
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-21 19:32:30 -0600

    createVenue functional

--- server/db/controllers/Venue.js
+++ server/db/controllers/Venue.js
@@ -1,24 +1,28 @@
 const { Venue } = require('../sequelize');
 
 // Create venue
-const createVenue = (req, res, next) => {
-    console.log('hey');
-    const { name, address1, address2, city, state, zip_code } = req.body;
-    Venue.create({
-        name,
-        address1,
-        address2,
-        city,
-        state,
-        zip_code
-    })
-    .then(() => console.log('success'))
-    .catch((err) => console.log(err));
-    next();
+const createVenue = (req, res) => {
+    try {
+        const { name, address1, address2, city, state, zip_code } = req.body;
+        Venue.create({
+            name,
+            address1,
+            address2,
+            city,
+            state,
+            zip_code
+        })
+        res.send(201);
+    }
+    catch (err) {
+        console.log(err);
+        res.send(400);
+    }
 }
 
 
-// Read venue
+// Get all venues
+// const getAllVenues = ()
 
 // Update venue
 
