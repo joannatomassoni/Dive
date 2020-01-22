f6e4ce5e0580070f04dca26bfd9892e9bc004519
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-21 19:06:02 -0600

    write createUser function to add user to db

---
+++ server/db/controllers/User.js
@@ -0,0 +1,93 @@
+const { User, Type, sequelize } = require('../sequelize');
+
+// Create user
+const createUser = async (req, res) => {
+    try {
+        const { name, typeName, bio, link_facebook, link_spotify, link_instagram, photo } = req.body;
+        const type = await Type.findOne({
+            where: {
+                typeName: typeName
+            }
+        });
+        User.create({
+            name,
+            typeId: type.id,
+            bio,
+            link_facebook,
+            link_instagram,
+            link_spotify
+        }).then(() => res.status(201).send('success'));
+    }
+    catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+}
+
+// Get single user
+const getSingleUser = async (req, res) => {
+    try {
+        const { name } = req.params;
+        const user = await User.findOrCreate({
+            where: {
+              name: name
+            }
+          })
+          res.status(200).send(user);
+    }
+    catch (err) {
+        res.send(err);        
+    }
+}
+
+// Get all fans
+const getAllFans = async (req, res, next) => {
+    try {
+        const { band } = req.body;
+        const sql = `SELECT * FROM users WHERE id IN 
+                        (SELECT id_fan FROM fan_band WHERE id_band IN (
+                            SELECT id from users WHERE name = ?
+                        ))`
+        const fans = await sequelize.query(sql, [band]);
+        res.status(200).send(fans);
+    }
+    catch (err) {
+        res.send(500, 'Failed to get fans');
+    }
+                // .catch((err) => {
+        //     console.log(err);
+        //     res.send(err);
+        // })
+}
+
+// Get single user
+
+// Get fans who have rsvpd to a show
+
+// Get fans who follow a given band
+
+// Get fans who follow a given venue
+
+// Update user
+
+// Delete user
+
+// Get all bands
+const getAllBands = (req, res) => {
+    User.findAll()
+        .then((users) => {
+            console.log(users);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
+}
+
+module.exports = {
+    createUser,
+    getSingleUser,
+    getAllFans,
+    getAllBands,
+}
--- server/db/controllers/index.js
+++ server/db/controllers/index.js
@@ -1,2 +1,12 @@
 // compile all controllers and export to router from here
-const { createUser } = require('./User');
+const { createUser, getAllFans, getAllBands } = require('./User');
+const { getTypes } = require('./Type');
+const { createVenue } = require('./Venue');
+
+module.exports = {
+    createUser,
+    getAllFans,
+    getTypes,
+    getAllBands,
+    createVenue
+}
