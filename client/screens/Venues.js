6924c81cd97b6fc06f04b49dc4b139f52f838ed9
Joy <greeniejoy@gmail.com>
2020-01-22 15:55:03 -0600

    front end venues get request getting data from db

--- client/screens/Venues.js
+++ client/screens/Venues.js
@@ -19,40 +19,55 @@ export default class Venues extends React.Component {
   }
 
   componentDidMount() {
-    console.log();
-    axios({
-      method: 'get',
-      // url: 'https://api.foursquare.com/v2/venues/search?near= new orleans, la&query=music venue&v=20150214&m=foursquare&client_secret=DGVIU40XZ35VVOJWJCEZEDYI5BWQNJIPDG3MCNK2N3AF1CLO&client_id=11ACWOWAADHLRUWGOKR4JCPTN3ENHFS25UVOHLKKYDYBIVWR',
-      url: 'https://api.foursquare.com/v2/venues/search',
-      params: {
-        client_id: '11ACWOWAADHLRUWGOKR4JCPTN3ENHFS25UVOHLKKYDYBIVWR',
-        client_secret: 'DGVIU40XZ35VVOJWJCEZEDYI5BWQNJIPDG3MCNK2N3AF1CLO',
-        v: '20180301',
-        query: 'music venue',
-        near: 'new orleans, la',
-        // ll: '29.9511, 90.0715',
-        limit: 3
-      }
-    })
+    console.log("hello");
+    // axios({
+    //   method: 'get',
+    //   // url: 'https://api.foursquare.com/v2/venues/search?near= new orleans, la&query=music venue&v=20150214&m=foursquare&client_secret=DGVIU40XZ35VVOJWJCEZEDYI5BWQNJIPDG3MCNK2N3AF1CLO&client_id=11ACWOWAADHLRUWGOKR4JCPTN3ENHFS25UVOHLKKYDYBIVWR',
+    //   url: 'https://api.foursquare.com/v2/venues/search',
+    //   params: {
+    //     client_id: '11ACWOWAADHLRUWGOKR4JCPTN3ENHFS25UVOHLKKYDYBIVWR',
+    //     client_secret: 'DGVIU40XZ35VVOJWJCEZEDYI5BWQNJIPDG3MCNK2N3AF1CLO',
+    //     v: '20180301',
+    //     query: 'music venue',
+    //     near: 'new orleans, la',
+    //     // ll: '29.9511, 90.0715',
+    //     limit: 3
+    //   }
+    // })
+    //   .then((response) => {
+    //     console.log("we're hitting api", response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]);
+    //     //this is hardcoded, gets the name of first music venue
+    //     this.setState({
+    //       name: response.data.response.venues[0].categories[0].pluralName,
+    //       address1: response.data.response.venues[0].location.address,
+    //       city: response.data.response.venues[0].location.city,
+    //       state: response.data.response.venues[0].location.formattedAddress[1].split(' ')[2],
+    //       zip_code: response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]
+    //     });
+    //     const name = response.data.response.venues[0].categories[0].pluralName;
+    //     const address = response.data.response.venues[0].location.address;
+    //     const city = response.data.response.venues[0].location.city;
+    //     const state = response.data.response.venues[0].location.formattedAddress[1].split(' ')[2];
+    //     const zip = response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]
+    //   })
+    //   .catch((err) => {
+    //     console.log("we're not hitting api", err);
+    //   })
+
+    axios.get('http://localhost:8080/venues')
       .then((response) => {
-        console.log("we're hitting api", response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]);
-        //this is hardcoded, gets the name of first music venue
         this.setState({
-          name: response.data.response.venues[0].categories[0].pluralName,
-          address1: response.data.response.venues[0].location.address,
-          city: response.data.response.venues[0].location.city,
-          state: response.data.response.venues[0].location.formattedAddress[1].split(' ')[2],
-          zip_code: response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]
+          name: response.data[4].name,
+          address1: response.data[4].address1,
+          city: response.data[4].city,
+          state: response.data[4].state,
+          zip_code: response.data[4].zip_code,
         });
-        const name = response.data.response.venues[0].categories[0].pluralName;
-        const address = response.data.response.venues[0].location.address;
-        const city = response.data.response.venues[0].location.city;
-        const state = response.data.response.venues[0].location.formattedAddress[1].split(' ')[2];
-        const zip = response.data.response.venues[0].location.formattedAddress[1].split(' ')[3]
+        console.log('UI is getting venues!', response.data[4].name);
+      }).catch(err => {
+        console.log('uh oh, not getting venues', err);
       })
-      .catch((err) => {
-        console.log("we're not hitting api", err);
-      })
+
   }
 
   render() {
@@ -61,7 +76,6 @@ export default class Venues extends React.Component {
       <View style={styles.container}>
         <MenuButton navigation={this.props.navigation} />
         <Text style={styles.text}>Venues</Text>
-
         <Text style={styles.text}>{name}</Text>
         <Text style={styles.text}>{address1}</Text>
         <Text style={styles.text}>{city}</Text>
