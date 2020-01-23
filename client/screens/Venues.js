2a12ea1b9e69f6fd331bc36b63c87a62a06ad2d5
Joy <greeniejoy@gmail.com>
2020-01-23 10:56:14 -0600

    changed venues to functional and updated to render 1 venue on page

--- client/screens/Venues.js
+++ client/screens/Venues.js
@@ -1,13 +1,45 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios';
-import { StyleSheet, Text, View, useEffect } from 'react-native';
-
-
-
 import MenuButton from '../components/MenuButton'
 
 // export default class Venues extends React.Component {
-export default function Shows(props) {
+export default function Venues(props) {
+  const [venues, setVenues] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/venues')
+      .then((response) => {
+        // this.setState({
+        setVenues(
+          {
+            name: response.data[4].name,
+            address1: response.data[4].address1,
+            city: response.data[4].city,
+            state: response.data[4].state,
+            zip_code: response.data[4].zip_code,
+          }
+        )
+        console.log('component mounted');
+      })
+    console.log('component mounted');
+  })
+
+  // axios.get('http://localhost:8080/venues')
+  //     .then((response) => {
+  //       // this.setState({
+  //       setVenues(
+  //         {
+  //           name: response.data[4].name,
+  //           address1: response.data[4].address1,
+  //           city: response.data[4].city,
+  //           state: response.data[4].state,
+  //           zip_code: response.data[4].zip_code,
+  //         }
+  //       )
+  //       console.log('component mounted');
+  //     })
   // constructor() {
   //   super()
   //   this.state = {
@@ -68,21 +100,18 @@ export default function Shows(props) {
   //   })
   // }
 
-  useEffect(() => {
-    console.log('component mounted');
-  })
 
-  render()
+
   // const { name, address1, city, state, zip_code } = this.state;
   return (
     <View style={styles.container}>
-      <MenuButton navigation={this.props.navigation} />
+      <MenuButton navigation={props.navigation} />
       <Text style={styles.text}>Venues</Text>
-      {/* <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>{address1}</Text>
-      <Text style={styles.text}>{city}</Text>
-      <Text style={styles.text}>{state}</Text>
-      <Text style={styles.text}>{zip_code}</Text> */}
+      <Text style={styles.text}>{venues.name}</Text>
+      <Text style={styles.text}>{venues.address1}</Text>
+      <Text style={styles.text}>{venues.city}</Text>
+      <Text style={styles.text}>{venues.state}</Text>
+      <Text style={styles.text}>{venues.zip_code}</Text>
 
     </View>
   )
