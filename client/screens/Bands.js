9c059d0c75c65e2bc6e054be0538abb9c9aa9285
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-23 15:20:14 -0600

    building out skeletons for views

--- client/screens/Bands.js
+++ client/screens/Bands.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 import { SignedInContext } from '../App'
 import MenuButton from '../components/MenuButton'
 
-export default function Bands (props) {
+export default function Bands(props) {
   //global user signin info and editing function
   const [userInfo, setUserInfo] = useContext(SignedInContext);
+  const [bands, setBands] = useState([]);
   //console.log(userInfo[0]);
-  
+  useEffect(() => {
+    axios.get('http://localhost:8080/bands')
+      .then((response) => {
+        console.log('hey');
+        console.log(response);
+        setBands(response.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
   return (
     <View style={styles.container}>
       <MenuButton navigation={props.navigation} />
@@ -15,6 +25,7 @@ export default function Bands (props) {
     </View>
   )
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- client/screens/Shows.js
+++ client/screens/Shows.js
@@ -3,21 +3,137 @@ import {
   StyleSheet, 
   Text, 
   View,
-  SafeAreaView,
-  SectionList, 
+  Image,
+  SafeAreaView
 } from 'react-native';
+import { 
+  Card,
+  ListItem,
+  Button,
+  Icon,
+} from 'react-native-elements'
 import { SignedInContext } from '../App'
 import MenuButton from '../components/MenuButton'
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function Shows(props) {
   ///global user signin info and editing function
   const [userInfo, setUserInfo] = useContext(SignedInContext);
 
+  //dummy data
+  const users = [
+    {
+      name: 'Ryan',
+      avatar: "https://lh3.googleusercontent.com/a-/AAuE7mAY3iahzehnNyuj1PJ8iiDn1zi8v7LFz7jB6dzcPw"
+    },
+]
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <MenuButton navigation={props.navigation}/>
+      <ScrollView style={{marginTop:30}}>
       <Text style={styles.text}>Shows</Text>
-    </View>
+
+      {/* implemented with Text and Button as children */}
+      <Card
+        title='SHOW TITLE HERE'
+        style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+        <Text style={{ marginBottom: 10 }}>
+          General information about the bands or specific show can go here.
+        </Text>
+        <Button
+          style={styles.button}
+          title='View More' />
+      </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+        {/* implemented with Text and Button as children */}
+        <Card
+          title='SHOW TITLE HERE'
+          style={styles.card}
+        // image={require('../images/pic2.jpg')}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            General information about the bands or specific show can go here.
+        </Text>
+          <Button
+            style={styles.button}
+            title='View More' />
+        </Card>
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -29,7 +145,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: 30,
-    color: '#fff'
-  }
+    fontSize: 50,
+    color: '#59C3D1',
+    opacity: 0.9,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    paddingRight: 20
+  },
+  card: {
+    backgroundColor: '#75A4AD',
+    borderRadius: 10
+  },
+  button: {
+    borderRadius: 5, 
+    marginHorizontal: 40, 
+    backgroundColor: '#59C3D1',
+  },
+  cardText: {
+
+  },
 })
