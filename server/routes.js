9e3de71d8511a4797ddff11b281624aa5bb27f1d
Joy <greeniejoy@gmail.com>
2020-01-23 19:32:02 -0600

    cleaned up code

--- server/routes.js
+++ server/routes.js
@@ -142,16 +142,12 @@ router.get('/fans/rsvps/:fanName', ctrl.getFanRSVPs)
 // TODO:
 // create a comment
 router.post('/comments/:id_user', ctrl.createComment);
-// router.post('/comments', function (req, res) {
-//   res.send("We received your comment info");
-// })
+
 
 // TODO: 
 // get all comments for a show
 router.get('/comments/:id_show', ctrl.getAllComments);
-// router.get('/comments/:show', function (req, res) {
-//   res.send("we are getting comment!", res);
-// })
+
 
 /**
  * TYPES
