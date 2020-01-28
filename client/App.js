9ace187b3b10b428df8fda161b0b9b472d5ebb4a
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-28 12:16:40 -0600

    removed map api from app

--- client/App.js
+++ client/App.js
@@ -2,15 +2,12 @@ import React, { createContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import DrawerNavigator from './navigation/DrawerNavigator';
 import { SignedInContext } from './context/UserContext';
-import {MAP_KEY} from 'react-native-dotenv'
 
 export default function App (props) {
   //initial login state of app
   const [userInfo, setUserInfo] = useState({});
   //disable yellow error boxes
   console.disableYellowBox = true;
-
-  console.log(MAP_KEY);
 
   return (
     <View style={styles.container}>
