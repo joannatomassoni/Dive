6bb79da32d0a435c94fee1859939e5f58fa8b95d
Joy <greeniejoy@gmail.com>
2020-02-01 15:42:36 -0600

    import header

--- client/App.js
+++ client/App.js
@@ -3,8 +3,9 @@ import { StyleSheet, View } from 'react-native';
 import DrawerNavigator from './navigation/DrawerNavigator';
 import { SignedInContext } from './context/UserContext';
 import { IOS_AUTH_KEY, ANDROID_AUTH_KEY, AXIOS_URL, MAP_KEY } from 'react-native-dotenv';
+import { Header } from './components/Header';
 
-export default function App (props) {
+export default function App(props) {
   //initial login state of app
   const [userInfo, setUserInfo] = useState({});
   //disable yellow error boxes
@@ -12,6 +13,9 @@ export default function App (props) {
 
   return (
     <View style={styles.container}>
+
+      <Header />
+
       {/* pass signIn state to all children components*/}
       <SignedInContext.Provider value={[userInfo, setUserInfo]}>
         <DrawerNavigator />
@@ -22,6 +26,6 @@ export default function App (props) {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
   }
 })
