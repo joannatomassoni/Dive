101153872e3695b50ec9dd90e189134b53ac7e81
Ryan Moragas <ryanmoragas@gmail.com>
2020-02-06 10:58:39 -0600

    fixed edit bio container

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 110,
-    marginTop: 160,
+    marginTop: 140,
     color: '#59C3D1',
     fontWeight: 'bold',
     fontFamily: 'AvenirNext-Bold',
