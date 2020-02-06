edb18b8e4f48568c57a96ac862e7f82811b81b29
Joy <greeniejoy@gmail.com>
2020-02-05 19:37:04 -0600

    merge change?

--- client/modals/SingleShowModal.js
+++ client/modals/SingleShowModal.js
@@ -192,17 +192,6 @@ export default function SingleShowModal(props) {
                       removeRsvp();
                     }}
                   >
-                    <Text style={styles.signupButtonText}>Add To Calendar</Text>
-                  </TouchableOpacity>
-                {/* button to rsvp to specific (shows when signed in) */}
-                {userInfo.signedIn ?
-                  //if already rsvp'd, show button to cancel rvp
-                  (rsvp ? <TouchableOpacity
-                    style={styles.cancelButtonContainer}
-                    onPress={() => {
-                      removeRsvp();
-                    }}
-                  >
                     <Text style={styles.signupButtonText}>Cancel RSVP</Text>
                   </TouchableOpacity>
                     //if not rsvp'd, show rsvp button
