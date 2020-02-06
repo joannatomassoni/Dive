7997c3eb0579fe8affeacdef0562341ad3dd33f3
Joy <greeniejoy@gmail.com>
2020-02-05 19:37:43 -0600

    change axios get request

--- client/screens/Shows.js
+++ client/screens/Shows.js
@@ -31,9 +31,9 @@ export default function Shows(props) {
 
   //request to get all shows
   const getAllShows = () => {
-    axios.get(`${AXIOS_URL}/shows`)
+    axios.get(`https://dive-266016.appspot.com/shows`)
       .then((response) => {
-        setShows(() => response.data); xw
+        setShows(() => response.data);
       })
       .catch((err) => {
         console.log(err);
@@ -91,60 +91,62 @@ export default function Shows(props) {
                       : null}
                     <Text style={styles.cardVenueText} key={show.venue.id}>{show.venue.name}</Text>
                   </View>
+                </View>
               </Card>
 
-                )
-              })}
-          <ShowsNearBy />
+            )
+          })}
+          {/* modal for getting nearby shows */}
+          {/* <ShowsNearBy /> */}
         </ScrollView>
       </LinearGradient>
     </View >
-      )
-    }
-    
+  )
+}
+
 const styles = StyleSheet.create({
-        container: {
-        flex: 1,
-      backgroundColor: '#2D323A',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#2D323A',
+    justifyContent: 'center',
+  },
   headerText: {
-        fontSize: 50,
-      color: '#3BAFBF',
-      fontWeight: 'bold',
-      textAlign: 'right',
-      paddingRight: 20,
-    },
+    fontSize: 50,
+    color: '#3BAFBF',
+    fontWeight: 'bold',
+    textAlign: 'right',
+    paddingRight: 20,
+  },
   button: {
-        borderRadius: 5,
-      marginHorizontal: 40,
-      backgroundColor: '#59C3D1',
-    },
+    borderRadius: 5,
+    marginHorizontal: 40,
+    backgroundColor: '#59C3D1',
+  },
   cardText: {
-        fontSize: 16,
-      color: '#59C3D1',
-      fontWeight: 'bold',
-      textAlign: 'left',
-      paddingRight: 20
-    },
+    fontSize: 16,
+    color: '#59C3D1',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingRight: 20
+  },
   cardVenueText: {
-        fontSize: 16,
-      color: '#AA8181',
-      fontWeight: 'bold',
-      textAlign: 'left',
-      paddingRight: 20
-    },
+    fontSize: 16,
+    color: '#AA8181',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingRight: 20
+  },
   card: {
-        borderWidth: 0,
-      paddingBottom: 0,
-      backgroundColor: '#111',
-      paddingBottom: 10
-    },
+    borderWidth: 0,
+    paddingBottom: 0,
+    backgroundColor: '#111',
+    paddingBottom: 10
+  },
   photo: {
-        width: 100,
-      height: 100,
-      borderRadius: 10
-    },
-  })
-  
-  
+    width: 100,
+    height: 100,
+    borderRadius: 10
+  },
+})
+
+
